7eded19dd890ea14d25f1e927979920522f7de30
konstantiin <Kostiicc@yandex.ru>
2024-11-26 18:01:48 +0300

    added tests

--- bin/for9.ml
+++
@@ -1,9 +0,0 @@
-(*using for loop*)
-let _ =
-  for a = 1 to 334 do
-    for b = a to 1000 - a do
-      let c = 1000 - a - b in
-      if (a * a) + (b * b) = c * c then
-        a * b * c |> string_of_int |> print_endline
-    done
-  done
--- bin/rec9.ml
+++
@@ -1,9 +0,0 @@
-(*example of not tail recursion, result in stack overflow*)
-let rec broot_force a b =
-  let c = 1000 - a - b in
-  let ans =
-    if a + b >= 1000 then broot_force (a + 1) (a + 1) else broot_force a (b + 1)
-  in
-  if c * c = (a * a) + (b * b) then a * b * c else ans
-
-let _ = broot_force 1 1 |> string_of_int |> print_endline
